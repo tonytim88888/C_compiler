d564f97007680463417548116ae34ce3405a6abb
Tony <tonytim88888@gmail.com>
2020-05-29 00:36:30 +0800

    Initial Commit

---
+++ CCC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CCC
+//
+//  Created by apple on 2020/5/29.
+//  Copyright © 2020年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
